dd0946dbcae1d61b97f40388d57ccff36f9e9365
aritya-arjunan <aritya.wps@gmail.com>
2021-10-03 10:01:14 +0900

    Added Chapter 3 and Chapter 4

---
+++ C4_first_calc.py
@@ -0,0 +1,34 @@
+def addition():
+    first = int(input('What is your first number? '))
+    second = int(input('What is your second number? '))
+    print(first + second)
+
+def subtraction():
+    first = int(input('What is your first number? '))
+    second = int(input('What is your second number? '))
+    print(first - second)
+
+
+
+def multiplication():
+    first = int(input('What is your first number? '))
+    second = int(input('What is your second number? '))
+    print(first * second)
+
+
+
+def division():
+    first = int(input('What is your first number? '))
+    second = int(input('What is your second number? '))
+    print(first / second)
+
+
+
+
+def modulo():
+    first = int(input('What is your first number? '))
+    second = int(input('What is your second number? '))
+    print(first % second)
+
+
+modulo()
